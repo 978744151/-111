2d33e2044974cb100ab3f4e3e59f99959e3cc886
Your Name <you@example.com>
2017-12-28 20:04:59 +0800

    my_全游

--- js/index.js
+++ js/index.js
@@ -1,54 +1,42 @@
-//-mui滚动初始化
-(function($){
-    $(".mui-scroll-wrapper").scroll({
-//            bounce: false,//滚动条是否有弹力默认是true
-//            indicators: false, //是否显示滚动条,默认是true
-    });
-})(mui);
-//    mui点击按钮初始化
-mui('body').on('tap','a',function(){document.location.href=this.href;});
-//    document.getElementById("backdrop").addEventListener('tap',function() {
-//        //阻止默认事件
-//        event.detail.gesture.preventDefault();
-//        // 移除焦点
-////        document.getElementById("searchtittle").blur();
-//    });
-//    swiper特效事件
-var mySwiper = new Swiper('.swiper-container',{
-    autoplay: 3000,//可选选项，自动滑动
-    speed:500,
-    loop: true,
-    direction: 'horizontal',
-    pagination : '.swiper-pagination',
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
+;$(function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable :true,
+        autoplay : 10000,
+        speed:1,
+        loop:true,
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
+    })
+    var mySwiper = new Swiper ('.swiper-container1', {
+        pagination: '.swiper-pagination',
+        paginationClickable :true,
+        autoplay : 10000,
+        speed:1,
+        loop:true,
 
-    onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
-//            swiperAnimateCache(swiper); //隐藏动画元素
-        swiperAnimate(swiper); //初始化完成开始动画
-        $('.fade').animate({
-            left:"50%",
-            opacity:"1",
-            top: "25%"
-        },1000, function () {
-            $('.slide').animate({
-                right:"50%",
-                opacity:"1"
-            },1000)
-        })
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
 
-    },
-    onSlideChangeEnd: function(swiper){
-        swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        // 如果需要前进后退按钮
+//        nextButton: '.swiper-button-next',
+//        prevButton: '.swiper-button-prev',
 
-    }
+        // 如果需要滚动条
+//        scrollbar: '.swiper-scrollbar',
+    })
 })
 
 //懒加载初始化
 $("img").lazyload({ threshold :400});
 
 //监听事件
-$(function(){
+;$(function(){
     var offsetTop =  $('.area').offset().top;
     var height = $(window).height()-100;
     //console.log(height)
